14347709d7490780ca52030d467701329be78473
LRCozier <lrcozier@gmail.com>
2025-06-06 20:56:49 +0100

    add contents to app.vue and home.vue

--- lacfitness/src/App.vue
+++ lacfitness/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <Layout>
-    <TopScroll />
-    <Section title="L.A.C Fitness" subtitle="Coming Soon">
-      <p class="site-text">My personal training services and custom training programmes will be available soon. Stay tuned!</p>
-    </Section>
-  </Layout>
+  <main>
+    <Layout>
+      <Section title="L.A.C Fitness" subtitle="Coming Soon">
+        <p class="site-text">My personal training services and custom training programmes will be available soon. Stay tuned!</p>
+      </Section>
+    </Layout>
+  </main>
+
 </template>
 
 <script lang="ts">
+
 import Layout from './components/Layout.vue';
-import TopScroll from './components/TopScroll.vue';
 import Section from './components/Section.vue';
 import './style.css';
 
