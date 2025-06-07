9b617079df33c07f26539f5acc025f4ee98fbdc8
LRCozier <lrcozier@gmail.com>
2025-06-07 19:57:39 +0100

    created comingsoon.vue and imported into app

--- lacfitness/src/App.vue
+++ lacfitness/src/App.vue
@@ -1,23 +1,15 @@
 <template>
-  <main>
-    <Layout>
-      <Section title="L.A.C Fitness" subtitle="Coming Soon">
-        <p class="site-text">My personal training services and custom training programmes will be available soon. Stay tuned!</p>
-      </Section>
-    </Layout>
-  </main>
-
+  <div id="app-container">
+    <TopScroll />
+    <ComingSoon /> 
+  </div>
 </template>
 
-<script lang="ts">
-
-import Layout from './components/Layout.vue';
-import Section from './components/Section.vue';
+<script setup lang="ts">
+import TopScroll from './components/TopScroll.vue';
+import ComingSoon from './views/ComingSoon.vue';
 import './style.css';
-
 </script>
 
-<style scoped>
-
-
+<style>
 </style>
---
+++ lacfitness/src/views/ComingSoon.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <Layout>
+      <Section title="L.A.C Fitness" subtitle="Coming Soon">
+        <p class="site-text">My personal training services and custom training programmes will be available soon. Stay tuned!</p>
+      </Section>
+    </Layout>
+  </main>
+</template>
+
+<script setup lang="ts">
+import Layout from '../components/Layout.vue';
+import Section from '../components/Section.vue'; 
+
+</script>
