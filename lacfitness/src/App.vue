2627fc7ebb6b98071100925adc24779811249d16
LRCozier <lrcozier@gmail.com>
2025-06-08 05:08:30 +0100

    removed top scroll from app.vue

--- lacfitness/src/App.vue
+++ lacfitness/src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div id="app-container">
-    <TopScroll />
     <ComingSoon /> 
   </div>
 </template>
 
 <script setup lang="ts">
-import TopScroll from './components/TopScroll.vue';
 import ComingSoon from './views/ComingSoon.vue';
 import './style.css';
 </script>
